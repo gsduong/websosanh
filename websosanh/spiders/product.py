b287b4b621e24f9a334b6b4165f289343e8e2615
gsduong <nguyenduong.ict.hust@gmail.com>
2016-12-21 14:45:04 +0700

    Fix typo

--- websosanh/spiders/product.py
+++ websosanh/spiders/product.py
@@ -6,22 +6,28 @@ class LazadaSpider(scrapy.Spider):
     allowed_domains = ["lazada.vn"]
     start_urls = ["http://www.lazada.vn/"]
 
-    def parse(self, response): # looking for links of parent categories
+    
+
+    def parse(self, response): 
         nav_wrapper = response.xpath('.//*[@class="c-second-menu__item c-second-menu__item_style_heading2"]/a/@href')
-        # Remove duplicate
+
         links = []
         for i in nav_wrapper:
-            if str(i.extract()) not in links:
+            if str(i.extract()) not in links: 
                 links.append(str(i.extract()))
 
-        links.pop() # Remove last item for error solving
+        links.pop() 
         for url in links:
-            yield scrapy.Request(url, callback=self.parse_all_pagination)   # each url is a link to a parent category, each category has many paginations
+            yield scrapy.Request(url, callback=self.parse_all_pagination)
 
 
     def parse_all_pagination(self, response):
 
-        nav_pagination = response.css("div.c-paging__wrapper > a::text")
+        nav_pagination = response.css("div.c-paging__wrapper > a::text") 
+        # 
+        # example.com/?page=1
+        # example.com/?page=2
+        # example.com/?page=3
 
         page_number = []
 
@@ -32,19 +38,18 @@ class LazadaSpider(scrapy.Spider):
         pagination_links = [response.urljoin("?page=")+`page` for page in range(min(page_number), max(page_number) + 1)]
 
         for url in pagination_links:
-            yield scrapy.Request(url, callback=self.parse_each_pagination)  # each pagination has many products
+            yield scrapy.Request(url, callback=self.parse_each_pagination)
 
-    def parse_each_pagination(self, response):                              # get links of all product in each pagination
-        for item in response.xpath('.//div[@class="product-card__name-wrap"]/span/text()'):
-            product = LazadaItem()
-            product['title'] = item.extract()
-            yield product
-            #yield scrapy.Request(url, callback=self.parse_product)          # call a callback to parse product in each product link
+    def parse_each_pagination(self, response):                              
+        for item in response.xpath('.//div[contains(@class,"product-card")]/a/@href'):
+            url = item.extract()
+            yield scrapy.Request(url, callback=self.parse_product)          
 
-    # def parse_product(self, response):
-    #     item = LazadaItem()
-
-    #     item['item_link'] = response.url
-    #     item['image_link'] = response.xpath('//*[@id="scj_product_detail"]//img[@id="scjZoom"]/@src').extract()
-        # item['description'] = response.xpath('//div[@class="info_wrap"]/*').extract()
-        # yield item
+    def parse_product(self, response):
+        item = LazadaItem()
+        item['title'] = response.xpath('.//title/text()').extract()         
+        
+    #   item['item_link'] = response.url
+    #   item['image_link'] = response.xpath('//*[@id="scj_product_detail"]//img[@id="scjZoom"]/@src').extract()
+    #   item['description'] = response.xpath('//div[@class="info_wrap"]/*').extract()
+        yield item
